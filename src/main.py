d59bf3d5339bd7fd6a15ff6a7aa6ca6c4c06d79a
vend11 <komarov.dima2021@gmail.com>
2026-03-06 00:25:11 +0300

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,31 @@
+import tkinter as tk
+from src.core.config import ConfigManager
+from src.core.state_manager import StateManager
+from src.core.events import EventSystem
+from src.database.db import DatabaseHelper
+from src.gui.main_window import MainWindow
+
+
+def main():
+    print(">>> Запуск CryptoSafe Manager <<<")
+
+    # 1. Инициализация ядра
+    config = ConfigManager(env='development')
+    events = EventSystem()
+
+    # ИСПРАВЛЕНО: Передаем events в StateManager
+    state = StateManager(events)
+
+    # 2. База данных
+    db = DatabaseHelper(db_path=config.get('db_path'))
+
+    # 3. Связка Config <-> DB
+    config.set_db(db)
+
+    # 4. Запуск GUI
+    app = MainWindow(config, state, db, events)
+    app.mainloop()
+
+
+if __name__ == "__main__":
+    main()
