b0e2cebde4832ad5f0ab0ad4b0604ed3de207574
vend11 <komarov.dima2021@gmail.com>
2026-03-05 17:00:56 +0300

    Добавлен XOR

--- src/core/crypto/placeholder.py
+++ src/core/crypto/placeholder.py
@@ -1,17 +1,10 @@
-import ctypes
 from .abstract import EncryptionService
 
-
 class AES256Placeholder(EncryptionService):
+    """Заглушка"""
     def encrypt(self, data: bytes, key: bytes) -> bytes:
-        result = bytes([b ^ key[i % len(key)] for i, b in enumerate(data)])
-        self._wipe(data)
-        return result
+        if not key: return data
+        return bytes([data[i] ^ key[i % len(key)] for i in range(len(data))])
 
     def decrypt(self, ciphertext: bytes, key: bytes) -> bytes:
         return self.encrypt(ciphertext, key)
-
-    def _wipe(self, data: bytes):
-        if data:
-            location = id(data) + 20
-            ctypes.memset(location, 0, len(data))
