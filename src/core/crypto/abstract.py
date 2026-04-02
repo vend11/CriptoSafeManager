05f2f4b0257dd420deb6bad11cdb4ee4eb57819c
vend11 <komarov.dima2021@gmail.com>
2026-04-03 00:58:58 +0300

    Поддержка KeyManager

--- src/core/crypto/abstract.py
+++ src/core/crypto/abstract.py
@@ -1,10 +1,18 @@
 from abc import ABC, abstractmethod
+from typing import Optional, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from src.core.key_manager import KeyManager
 
 class EncryptionService(ABC):
     @abstractmethod
-    def encrypt(self, data: bytes, key: bytes) -> bytes:
+    def set_key_manager(self, key_manager: "KeyManager"):
         pass
 
     @abstractmethod
-    def decrypt(self, ciphertext: bytes, key: bytes) -> bytes:
+    def encrypt(self, data: bytes, key: Optional[bytes] = None) -> bytes:
+        pass
+
+    @abstractmethod
+    def decrypt(self, ciphertext: bytes, key: Optional[bytes] = None) -> bytes:
         pass
