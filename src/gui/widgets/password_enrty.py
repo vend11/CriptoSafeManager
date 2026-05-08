2f0f3127039108829374ad001dc8ca950d47254c
vend11 <komarov.dima2021@gmail.com>
2026-05-08 10:11:17 +0300

    Update password_enrty.py

--- src/gui/widgets/password_enrty.py
+++ src/gui/widgets/password_enrty.py
@@ -1,26 +1,36 @@
 import tkinter as tk
+from tkinter import ttk
 
 
-class PasswordEntry(tk.Frame):
-    """Поле пароля с кнопкой 'глаз'"""
+class PasswordEntry(ttk.Frame):
+    """
+    Виджет для ввода пароля с возможностью отображения/скрытия текста.
+    """
 
     def __init__(self, parent, **kwargs):
         super().__init__(parent)
-        self.var = tk.StringVar()
 
-        self.entry = tk.Entry(self, textvariable=self.var, show="*", **kwargs)
+        self.show_var = tk.BooleanVar(value=True)
+
+        # Поле ввода
+        self.entry = ttk.Entry(self, show="*", **kwargs)
         self.entry.pack(side=tk.LEFT, fill=tk.X, expand=True)
 
-        self.btn = tk.Button(self, text="👁", width=3, command=self.toggle)
-        self.btn.pack(side=tk.RIGHT)
+        # Кнопка показать
+        self.toggle_btn = ttk.Button(self, text="👁", width=3, command=self.toggle_visibility)
+        self.toggle_btn.pack(side=tk.RIGHT, padx=(5, 0))
 
-    def toggle(self):
-        if self.entry.cget('show') == '*':
-            self.entry.config(show='')
-            self.btn.config(text="🔒")
+    def toggle_visibility(self):
+        if self.show_var.get():
+            self.entry.config(show="")
+            self.show_var.set(False)
         else:
-            self.entry.config(show='*')
-            self.btn.config(text="👁")
+            self.entry.config(show="*")
+            self.show_var.set(True)
 
     def get(self):
-        return self.var.get()
+        return self.entry.get()
+
+    def set(self, value):
+        self.entry.delete(0, tk.END)
+        self.entry.insert(0, value)
