268f997381bfbdc8fb6207074ad36bf80a32b9f2
vend11 <komarov.dima2021@gmail.com>
2026-05-08 09:56:51 +0300

    -

--- src/core/crypto/placeholder.py
+++ src/core/crypto/placeholder.py
@@ -1,28 +1,16 @@
 from .abstract import EncryptionService
-from typing import TYPE_CHECKING, Optional
-
-if TYPE_CHECKING:
-    from src.core.key_manager import KeyManager
 
 
 class AES256Placeholder(EncryptionService):
-    def __init__(self):
-        self._key_manager = None
+    def encrypt(self, data: bytes) -> bytes:
+        # Используем ключ из key_manager
+        key = self.key_manager.storage.get_key()
+        if not key:
+            raise ValueError("Key not set")
 
-    def set_key_manager(self, key_manager: "KeyManager"):
-        self._key_manager = key_manager
+        # XOR Заглушка
+        key_len = len(key)
+        return bytes([data[i] ^ key[i % key_len] for i in range(len(data))])
 
-    def encrypt(self, data: bytes, key: Optional[bytes] = None) -> bytes:
-        target_key = key
-        if target_key is None:
-            if not self._key_manager:
-                raise RuntimeError("KeyManager not set and no key provided")
-            target_key = self._key_manager.get_session_key()
-
-        if not target_key:
-            raise RuntimeError("Encryption key is missing")
-
-        return bytes([data[i] ^ target_key[i % len(target_key)] for i in range(len(data))])
-
-    def decrypt(self, ciphertext: bytes, key: Optional[bytes] = None) -> bytes:
-        return self.encrypt(ciphertext, key)
+    def decrypt(self, ciphertext: bytes) -> bytes:
+        return self.encrypt(ciphertext)
